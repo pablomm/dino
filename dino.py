cefd1f744f20c5fc10fb82f0e37f59c2d49c9fdc
pablomm <pablo.marcosm@estudiante.uam.es>
2018-07-06 16:29:40 +0200

    Random dino finished

--- dino.py
+++ dino.py
@@ -68,6 +68,13 @@ class Dino:
         self._driver.execute_script(
             "Runner.keycodes['DUCK'] = {'40' : 1, '16' : 1};")
 
+        # Width of the Pterodactyl used for the duck
+        self.pterodactylx = int(self.getProperty(
+            "Runner.spriteDefinition.LDPI.PTERODACTYL.x"))
+
+        # Configuration params of the game
+        self.gameConfig = self.getProperty("Runner.config")
+
         # Rest of initialization stuff
         self.body = self._driver.find_element(By.TAG_NAME, "body")
         self.canvas = self._driver.find_element(By.ID, canvasID)
@@ -93,21 +100,26 @@ class Dino:
     def jump(self):
         """Make the dino jump."""
 
-        print("Jumping")
         self.body.send_keys(Keys.SPACE)
 
-    def duck(self, delay=.5):
+    def duck(self, delay=None):
         """Make the dino duck.
             TODO: Adjust default delay to the current speed.
 
             Args:
                 delay (float, optional): Seconds ducking
         """
-        print("Duck")
+
+        if delay == None:
+            delay = (self.pterodactylx+20)/self.speed
+            delay = .5
 
         ActionChains(self._driver).key_down(Keys.SHIFT).perform()
         time.sleep(delay)
         ActionChains(self._driver).key_up(Keys.SHIFT).perform()
+
+    def cancelJump(self):
+        self.body.send_keys(Keys.ARROW_DOWN)
 
     def pause(self):
         return self._driver.execute_script("return Runner.instance_.stop()")
@@ -116,52 +128,79 @@ class Dino:
         return self._driver.execute_script("return Runner.instance_.play()")
 
     def end(self):
-        self._driver.close()
+        try:
+            self._driver.close()
+        except:
+            pass
+
+    def getProperty(self, property):
+        """Execute JS code to get the property requested, i.e."""
+        return self._driver.execute_script("return {}".format(property))
 
     @property
     def crashed(self):
         """True if the dino is crashed"""
-        return self._driver.execute_script("return Runner.instance_.crashed")
+        return self.getProperty("Runner.instance_.crashed")
 
     @property
     def playing(self):
         """True if the dino is playing"""
-        return self._driver.execute_script("return Runner.instance_.playing")
+        return self.getProperty("Runner.instance_.playing")
 
     @property
     def score(self):
         """Return the score"""
-        score_array = self._driver.execute_script(
-            "return Runner.instance_.distanceMeter.digits")
+        score_array = self.getProperty("Runner.instance_.distanceMeter.digits")
         score = ''.join(score_array)
         return int(score)
 
     @property
     def speed(self):
         """Return the speed"""
-        return float(self._driver.execute_script("return Runner.instance_.currentSpeed"))
+        return float(self.getProperty("Runner.instance_.currentSpeed"))
 
     @property
     def image(self):
         """Return the dino image png encoding in base64"""
-        return self._driver.execute_script("canvasRunner = document.getElementById('runner-canvas'); \
-return canvasRunner.toDataURL().substring(22)")
+        return self._driver.execute_script(
+            "canvasRunner = document.getElementById('runner-canvas');\
+            return canvasRunner.toDataURL().substring(22)")
+
+    @property
+    def obstacles(self):
+        print("Obstacles")
+        """Return a tuple with the distance of each object"""
+        return self._driver.execute_script("obst = [];\
+                                           Runner.instance_.horizon.obstacles.forEach(\
+                                           function(e){\
+                                           e = Object.assign({},e);\
+                                           delete e.canvasCtx;\
+                                           obst.push(e)});\
+                                           return obst")
+
+    @property
+    def tRex(self):
+        """Extract all the info needed to known the state of the game"""
+        return self._driver.execute_script("trex = Object.assign({},Runner.instance_.tRex);\
+                                           delete trex.canvas;\
+                                           delete trex.canvasCtx;\
+                                           delete trex.currentAnimFrames;\
+                                           delete trex.config;\
+                                           return trex")
 
 
 if __name__ == '__main__':
 
     import random
 
+    random.seed()
     dino = Dino().start()
 
     # Dummy random behaviour
-    random.seed()
-
     try:
         while True:
-            print(dino.speed)
-            random.choice([dino.duck, dino.jump, dino.jump])()
-            time.sleep(0.7)
-            print(dino.image)
-    except Exception:
+            random.choice(10*[dino.jump] + [dino.duck, dino.cancelJump])()
+            time.sleep(0.6)
+
+    except:
         dino.end()
